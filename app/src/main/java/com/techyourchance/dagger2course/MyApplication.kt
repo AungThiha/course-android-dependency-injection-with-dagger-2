be2b5343a61de2a22b311b445041f686bb3d73eb
Aung Thiha <mr.aungthiha@gmail.com>
2021-01-01 22:56:40 +0700

    Refactoring FetchQuestionDetailsUseCase construction into MyApplication

--- app/src/main/java/com/techyourchance/dagger2course/MyApplication.kt
+++ app/src/main/java/com/techyourchance/dagger2course/MyApplication.kt
@@ -2,6 +2,7 @@ package com.techyourchance.dagger2course
 
 import android.app.Application
 import com.techyourchance.dagger2course.networking.StackoverflowApi
+import com.techyourchance.dagger2course.questions.FetchQuestionDetailsUseCase
 import com.techyourchance.dagger2course.questions.FetchQuestionsUseCase
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -14,9 +15,12 @@ class MyApplication : Application() {
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
-    val stackoverflowApi: StackoverflowApi = retrofit.create(StackoverflowApi::class.java)
+    private val stackoverflowApi: StackoverflowApi = retrofit.create(StackoverflowApi::class.java)
+
     val fetchQuestionsUseCase: FetchQuestionsUseCase
         get() = FetchQuestionsUseCase(stackoverflowApi)
+    val fetchQuestionDetailsUseCase: FetchQuestionDetailsUseCase
+        get() = FetchQuestionDetailsUseCase(stackoverflowApi)
 
     override fun onCreate() {
         super.onCreate()
