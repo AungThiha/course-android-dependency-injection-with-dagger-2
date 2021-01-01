ae93799cf86f1a9f15c03b656af8100bbd139524
Aung Thiha <mr.aungthiha@gmail.com>
2021-01-01 21:45:39 +0700

    Sharing a single instance of Retrofit between all clients

--- app/src/main/java/com/techyourchance/dagger2course/questions/FetchQuestionDetailsUseCase.kt
+++ app/src/main/java/com/techyourchance/dagger2course/questions/FetchQuestionDetailsUseCase.kt
@@ -1,26 +1,20 @@
 package com.techyourchance.dagger2course.questions
 
-import com.techyourchance.dagger2course.Constants
 import com.techyourchance.dagger2course.networking.StackoverflowApi
 import com.techyourchance.dagger2course.questions.FetchQuestionDetailsUseCase.Result.*
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
-class FetchQuestionDetailsUseCase {
+class FetchQuestionDetailsUseCase(
+        private val retrofit: Retrofit
+) {
 
     sealed class Result {
         class Success(val questionWithBody: QuestionWithBody) : Result()
         object Failure: Result()
     }
-
-    // init retrofit
-    private val retrofit = Retrofit.Builder()
-            .baseUrl(Constants.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
 
     private val stackoverflowApi: StackoverflowApi = retrofit.create(StackoverflowApi::class.java)
 
