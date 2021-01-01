9cd7af62f2697269303bddf3d7ed0972cf35594a
Aung Thiha <mr.aungthiha@gmail.com>
2021-01-01 15:00:15 +0700

    Extraction of BaseViewMvc

--- app/src/main/java/com/techyourchance/dagger2course/screens/questiondetails/QuestionDetailsViewMvc.kt
+++ app/src/main/java/com/techyourchance/dagger2course/screens/questiondetails/QuestionDetailsViewMvc.kt
@@ -3,51 +3,35 @@ package com.techyourchance.dagger2course.screens.questiondetails
 import android.os.Build
 import android.text.Html
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.annotation.IdRes
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.techyourchance.dagger2course.R
 import com.techyourchance.dagger2course.screens.common.toolbar.MyToolbar
+import com.techyourchance.dagger2course.screens.common.viewsmvc.BaseViewMvc
 
 class QuestionDetailsViewMvc(
-        private val layoutInflater: LayoutInflater,
-        private val parent: ViewGroup?
-) {
+        layoutInflater: LayoutInflater,
+        parent: ViewGroup?
+): BaseViewMvc<QuestionDetailsViewMvc.Listener>(layoutInflater, parent, R.layout.layout_question_details) {
 
     interface Listener {
         fun onBackClicked()
     }
 
-    private var toolbar: MyToolbar
-    private var swipeRefresh: SwipeRefreshLayout
-    private var txtQuestionBody: TextView
-
-    val rootView: View = layoutInflater.inflate(R.layout.layout_question_details, parent, false)
-    private val listeners = HashSet<Listener>()
+    private val toolbar: MyToolbar = findViewById(R.id.toolbar)
+    private val swipeRefresh: SwipeRefreshLayout = findViewById(R.id.swipeRefresh)
+    private val txtQuestionBody: TextView = findViewById(R.id.txt_question_body)
 
     init {
 
-        txtQuestionBody = findViewById(R.id.txt_question_body)
-
         // init toolbar
-        toolbar = findViewById(R.id.toolbar)
         toolbar.setNavigateUpListener {
             listeners.forEach { it.onBackClicked() }
         }
 
         // init pull-down-to-refresh (used as a progress indicator)
-        swipeRefresh = findViewById(R.id.swipeRefresh)
         swipeRefresh.isEnabled = false
-    }
-
-    fun registerListener(listener: Listener) {
-        listeners.add(listener)
-    }
-
-    fun unregisterListener(listener: Listener) {
-        listeners.remove(listener)
     }
 
     fun showProgressIndication() {
@@ -65,10 +49,6 @@ class QuestionDetailsViewMvc(
             @Suppress("DEPRECATION")
             txtQuestionBody.text = Html.fromHtml(questionBody)
         }
-    }
-
-    fun <T : View?> findViewById(@IdRes id: Int): T {
-        return rootView.findViewById<T>(id)
     }
 
 }
--- app/src/main/java/com/techyourchance/dagger2course/screens/questionslist/QuestionListViewMvc.kt
+++ app/src/main/java/com/techyourchance/dagger2course/screens/questionslist/QuestionListViewMvc.kt
@@ -1,48 +1,39 @@
 package com.techyourchance.dagger2course.screens.questionslist
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.annotation.IdRes
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.techyourchance.dagger2course.R
 import com.techyourchance.dagger2course.questions.Question
+import com.techyourchance.dagger2course.screens.common.viewsmvc.BaseViewMvc
 import java.util.ArrayList
 
 class QuestionListViewMvc(
-        private val layoutInflater: LayoutInflater,
-        private val parent: ViewGroup?
-) {
+        layoutInflater: LayoutInflater,
+        parent: ViewGroup?
+): BaseViewMvc<QuestionListViewMvc.Listener>(layoutInflater, parent, R.layout.layout_questions_list) {
 
     interface Listener {
         fun onRefreshClicked()
         fun onQuestionClicked(clickedQuestion: Question)
     }
 
-    private val swipeRefresh: SwipeRefreshLayout
-    private val recyclerView: RecyclerView
+    private val swipeRefresh: SwipeRefreshLayout = findViewById(R.id.swipeRefresh)
+    private val recyclerView: RecyclerView = findViewById(R.id.recycler)
     private val questionsAdapter: QuestionsAdapter
-
-    val rootView: View = layoutInflater.inflate(R.layout.layout_questions_list, parent, false)
-    private val context: Context
-        get() { return rootView.context }
-
-    private val listeners = HashSet<Listener>()
 
     init {
 
         // init pull-down-to-refresh
-        swipeRefresh = findViewById(R.id.swipeRefresh)
         swipeRefresh.setOnRefreshListener {
             listeners.forEach { it.onRefreshClicked() }
         }
 
         // init recycler view
-        recyclerView = findViewById(R.id.recycler)
         recyclerView.layoutManager = LinearLayoutManager(context)
         questionsAdapter = QuestionsAdapter{ clickedQuestion ->
             listeners.forEach { it.onQuestionClicked(clickedQuestion) }
@@ -63,18 +54,6 @@ class QuestionListViewMvc(
         if (swipeRefresh.isRefreshing) {
             swipeRefresh.isRefreshing = false
         }
-    }
-
-    fun <T : View?> findViewById(@IdRes id: Int): T {
-        return rootView.findViewById<T>(id)
-    }
-
-    fun registerListener(listener: Listener) {
-        listeners.add(listener)
-    }
-
-    fun unregisterListener(listener: Listener) {
-        listeners.remove(listener)
     }
 
     class QuestionsAdapter(
