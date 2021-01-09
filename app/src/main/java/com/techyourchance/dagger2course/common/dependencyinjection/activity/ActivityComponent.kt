d6b85c3b5bedfa886efe4591dfbb30bcaafbb773
Aung Thiha <mr.aungthiha@gmail.com>
2021-01-09 23:56:53 +0700

    Refactoring PresentationCompoenent to become subcomponent of Activity Component

--- app/src/main/java/com/techyourchance/dagger2course/common/dependencyinjection/activity/ActivityComponent.kt
+++ app/src/main/java/com/techyourchance/dagger2course/common/dependencyinjection/activity/ActivityComponent.kt
@@ -1,28 +1,15 @@
 package com.techyourchance.dagger2course.common.dependencyinjection.activity
 
-import android.app.Application
-import android.view.LayoutInflater
-import androidx.fragment.app.FragmentManager
+
 import com.techyourchance.dagger2course.common.dependencyinjection.app.AppComponent
-import com.techyourchance.dagger2course.networking.StackoverflowApi
-import com.techyourchance.dagger2course.screens.common.ScreensNavigator
-import com.techyourchance.dagger2course.screens.common.activities.BaseActivity
+import com.techyourchance.dagger2course.common.dependencyinjection.presentation.PresentationComponent
+import com.techyourchance.dagger2course.common.dependencyinjection.presentation.PresentationModule
 import dagger.Component
 
 @ActivityScope
 @Component(dependencies = [AppComponent::class], modules = [ActivityModule::class])
 interface ActivityComponent {
 
-    fun activity(): BaseActivity
-
-    fun screensNavigator(): ScreensNavigator
-
-    fun application(): Application
-
-    fun fragmentManager(): FragmentManager
-
-    fun layoutInflater(): LayoutInflater
-
-    fun stackoverflowApi(): StackoverflowApi
+    fun newPresentationComponent(presentationModule: PresentationModule): PresentationComponent
 
 }
--- app/src/main/java/com/techyourchance/dagger2course/screens/common/activities/BaseActivity.kt
+++ app/src/main/java/com/techyourchance/dagger2course/screens/common/activities/BaseActivity.kt
@@ -5,7 +5,6 @@ import com.techyourchance.dagger2course.MyApplication
 import com.techyourchance.dagger2course.common.dependencyinjection.activity.ActivityComponent
 import com.techyourchance.dagger2course.common.dependencyinjection.activity.ActivityModule
 import com.techyourchance.dagger2course.common.dependencyinjection.activity.DaggerActivityComponent
-import com.techyourchance.dagger2course.common.dependencyinjection.presentation.DaggerPresentationComponent
 import com.techyourchance.dagger2course.common.dependencyinjection.presentation.PresentationComponent
 import com.techyourchance.dagger2course.common.dependencyinjection.presentation.PresentationModule
 
@@ -21,10 +20,7 @@ abstract class BaseActivity : AppCompatActivity() {
     }
 
     private val presentationComponent: PresentationComponent by lazy {
-        DaggerPresentationComponent.builder()
-                .activityComponent(activityComponent)
-                .presentationModule(PresentationModule())
-                .build()
+       activityComponent.newPresentationComponent(PresentationModule())
     }
 
     val injector get() = presentationComponent
